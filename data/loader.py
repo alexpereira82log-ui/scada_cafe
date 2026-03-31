2fa9f679011b630bc4cb2a343b083705e195a935
Alex Pereira <alex.pereira82log@gmail.com>
2026-03-31 00:34:42 -0300

    update

--- data/loader.py
+++ data/loader.py
@@ -1,17 +1,36 @@
 import pandas as pd
 from database.connection import get_connection
 
+
 def carregar_dados():
     """
     Carrega todas as tabelas do banco e retorna um dicionário de DataFrames.
     """
 
-    with get_connection() as conn:
-        dados = {
-            "base_fat": pd.read_sql("SELECT * FROM base_fat", conn),
-            "base_comissao": pd.read_sql("SELECT * FROM comissao_colaborador", conn),
-            "base_perdas": pd.read_sql("SELECT * FROM perdas", conn),
-            "base_produtos": pd.read_sql("SELECT * FROM venda_produtos", conn),
-        }
+    conn = get_connection()
+
+    dados = {}
+
+    try:
+        dados["base_fat"] = pd.read_sql("SELECT * FROM base_fat", conn)
+    except:
+        dados["base_fat"] = pd.DataFrame()
+
+    try:
+        dados["base_comissao"] = pd.read_sql("SELECT * FROM comissao_colaborador", conn)
+    except:
+        dados["base_comissao"] = pd.DataFrame()
+
+    try:
+        dados["base_perdas"] = pd.read_sql("SELECT * FROM perdas", conn)
+    except:
+        dados["base_perdas"] = pd.DataFrame()
+
+    try:
+        dados["base_produtos"] = pd.read_sql("SELECT * FROM venda_produtos", conn)
+    except:
+        dados["base_produtos"] = pd.DataFrame()
+
+    conn.close()
 
     return dados
